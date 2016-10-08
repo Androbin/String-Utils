3d0e1ef60f56ca50a7ff854176b14ff16e4013a8
Androbin <robin.richtsfeld@gmail.com>
2016-10-08 13:19:15 +0200

    Create FormatUtil.java

---
+++ src/de/androbin/util/txt/FormatUtil.java
@@ -0,0 +1,13 @@
+package de.androbin.util.txt;
+
+public final class FormatUtil
+{
+	private FormatUtil()
+	{
+	}
+	
+	public static String trim( final String s, final int n )
+	{
+		return s.length() <= n ? s : s.substring( 0, n - 3 ) + "...";
+	}
+}
