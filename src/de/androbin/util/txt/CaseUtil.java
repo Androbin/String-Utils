84fd033028178ad1c9165dc6669ae69426a7e567
Androbin <robin.richtsfeld@gmail.com>
2017-05-18 20:39:22 +0200

    updated formatting

--- src/de/androbin/util/txt/CaseUtil.java
+++ src/de/androbin/util/txt/CaseUtil.java
@@ -2,34 +2,29 @@ package de.androbin.util.txt;
 
 import java.util.*;
 
-public final class CaseUtil
-{
-	private CaseUtil()
-	{
-	}
-	
-	public static String toLowerCamelCase( final String sentence )
-	{
-		final String[] words = sentence.split( "_" );
-		final Iterator<String> iter = Arrays.stream( words ).iterator();
-		final StringBuilder sb = new StringBuilder( iter.next().toLowerCase() );
-		
-		iter.forEachRemaining( word -> sb.append( toProperCase( iter.next() ) ) );
-		return sb.toString();
-	}
-	
-	public static String toProperCase( final String word )
-	{
-		return word.substring( 0, 1 ).toUpperCase() + word.substring( 1 ).toLowerCase();
-	}
-	
-	public static String toUpperCamelCase( final String sentence )
-	{
-		final String[] words = sentence.split( "_" );
-		final Iterator<String> iter = Arrays.stream( words ).iterator();
-		final StringBuilder sb = new StringBuilder();
-		
-		iter.forEachRemaining( word -> sb.append( toProperCase( iter.next() ) ) );
-		return sb.toString();
-	}
+public final class CaseUtil {
+  private CaseUtil() {
+  }
+  
+  public static String toLowerCamelCase( final String sentence ) {
+    final String[] words = sentence.split( "_" );
+    final Iterator<String> iter = Arrays.stream( words ).iterator();
+    final StringBuilder sb = new StringBuilder( iter.next().toLowerCase() );
+    
+    iter.forEachRemaining( word -> sb.append( toProperCase( iter.next() ) ) );
+    return sb.toString();
+  }
+  
+  public static String toProperCase( final String word ) {
+    return word.substring( 0, 1 ).toUpperCase() + word.substring( 1 ).toLowerCase();
+  }
+  
+  public static String toUpperCamelCase( final String sentence ) {
+    final String[] words = sentence.split( "_" );
+    final Iterator<String> iter = Arrays.stream( words ).iterator();
+    final StringBuilder sb = new StringBuilder();
+    
+    iter.forEachRemaining( word -> sb.append( toProperCase( iter.next() ) ) );
+    return sb.toString();
+  }
 }
--- src/de/androbin/util/txt/FormatUtil.java
+++ src/de/androbin/util/txt/FormatUtil.java
@@ -1,13 +1,10 @@
 package de.androbin.util.txt;
 
-public final class FormatUtil
-{
-	private FormatUtil()
-	{
-	}
-	
-	public static String trim( final String s, final int n )
-	{
-		return s.length() <= n ? s : s.substring( 0, n - 3 ) + "...";
-	}
+public final class FormatUtil {
+  private FormatUtil() {
+  }
+  
+  public static String trim( final String s, final int n ) {
+    return s.length() <= n ? s : s.substring( 0, n - 3 ) + "...";
+  }
 }
